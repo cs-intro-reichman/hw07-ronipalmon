c41c5bb484afd3860d7740f1123bfa3a1aa22ab3
Roni Palmon <roni.palmon@post.runi.ac.il>
2024-02-01 19:30:14 +0200

    all

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,69 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 0){
+			return "";
+		}else{
+			str = str.substring(1);
+		}
+		return str;
+		
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		int distance = 0;
+		word1=word1.toLowerCase();
+		word2=word2.toLowerCase();
+
+		if (word2.length() == 0){
+			distance= word1.length();
+			return distance;
+		}
+		if (word1.length()== 0){
+			distance= word2.length();
+			return distance;
+		}
+		if (word1.charAt(0)==word2.charAt(0)){
+			distance = levenshtein(tail(word1), tail(word2));
+			return distance;
+		}
+		else{
+			int temp1= levenshtein(tail(word1), word2);
+			int temp2 = levenshtein(word1, tail(word2));
+			int temp3 = levenshtein(tail(word1), tail(word2));
+			distance = Math.min(Math.min(temp1,temp2),temp3) +1;
+			return distance;
+		}
 	}
+
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i=0; i<3000; i++){
+			dictionary[i]=in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
-	}
+		int distance = levenshtein(word, dictionary[0]);
+		int index = 0;
+		
+		for (int i=1; i < dictionary.length; i++){
+			if (levenshtein(word, dictionary[i])<=distance){
+				distance = (levenshtein(word, dictionary[i]));
+				index = i;
+			} 
+			
+		}
+		if (distance <= threshold){
+			return dictionary[index];
+	} 
+		return word;
 
+}
 }
